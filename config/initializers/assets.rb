3eff931bfc1bf594285b97734c9c0a0b7d823ab9
Matt Johnson <matt@mattjohnson.io>
2014-12-04 10:49:23 +0000

    Correctly include icon fonts.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,14 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if File.basename(path).starts_with?('_') || path !~ /\.(css|js|afm|eot|ttf|woff)\z/
+    false
+  else
+    full_path          = Rails.application.assets.resolve(path).to_path
+    app_assets_path    = Rails.root.join('app', 'assets').to_path
+    vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+
+    full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path)
+  end
+end
